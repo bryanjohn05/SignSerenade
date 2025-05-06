29fc2ffd57f095be743b2fa91f3213bb1bdfddbf
Bryan Sohan John <bryansohanjohn@gmail.com>
2025-05-06 22:43:41 +0530

    frontend deleted

--- frontend/app/real-time/page.tsx
+++
@@ -1,9 +0,0 @@
-import MediapipeSignDetector from "@/components/mediapipe-sign-detector"
-
-export default function RealTimePage() {
-  return (
-    <div className="container mx-auto py-8">
-      <MediapipeSignDetector />
-    </div>
-  )
-}
